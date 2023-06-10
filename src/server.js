a5a7d2d1f5bcb2b6e987445912faa8c49ae4cdc3
Leandro Basignani <leandrobasignani@gmail.com>
2023-06-09 22:32:37 -0300

    final test version CORS and ENV

--- src/server.js
+++ src/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
 
+
+
+try {
+  dotenv.config({ path: process.cwd() + '/' + '.env' });
+} catch (e) {
+}
 //settings
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT);
 const whiteList = ['http://localhost','https://fluttertestappnoti.azurewebsites.net']
 
 const dbConfig = {
@@ -22,11 +29,13 @@ const dbConfig = {
 // app.use(cors({
 //   origin:whiteList
 // }));
+
+app.use(cors());
 app.use(bodyParser.json()) 
 // Rutas de users
 const usersRoutes = require('./routes/users')(dbConfig);
 app.use('/users', usersRoutes);
-app.use('/users', usersRoutes);
+// app.use('/users', usersRoutes);
 
 app.listen(app.get('port'), () => {
   console.log('Servidor iniciado en el puerto', app.get('port'));
